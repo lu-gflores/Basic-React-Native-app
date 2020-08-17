bb4fdfc8f937404c5ae61ef065a7b171227fb10d
George Flores <georgeflores886@gmail.com>
2020-08-17 11:29:34 -0400

    rendered list items and remove icon

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, FlatList} from 'react-native'
 import Header from './components/Header'
+import ListItem from './components/ListItem'
+import {uuidv4, uuid} from 'uuidv4'
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'milk'},
+    {id: uuid(), text: 'bread'},
+    {id: uuid(), text: 'eggs'},
+    {id: uuid(), text: 'donuts'},
+
+  ]);
   return (
     <View style={styles.container}>
       <Header/>
+      <FlatList data={items} renderItem={({item}) => (
+       <ListItem item={item}/>
+      )}/>
     </View>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.text}>Shopping List</Text>
+      <Text style={styles.text}>{title}</Text>
     </View>
   )
+}
+Header.defaultProps = {
+    title: 'Shopping List'
 }
 const styles = StyleSheet.create({
   header: {
