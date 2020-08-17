9dd72b0f7655f857d59d71015e079bc5db74ddb2
George Flores <georgeflores886@gmail.com>
2020-08-17 10:23:05 -0400

    starting initial files

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style= {styles.text}>
+        Hello World!
+      </Text>
     </View>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
   },
-});
+  text: {
+    color: 'darkslateblue',
+     fontSize: 30
+  }
+})
+export default App;
